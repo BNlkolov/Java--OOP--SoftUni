aa423399c3916a3fd8b62c39c7db6d650e323ced
Borislav Nikolov <138274747+BNlkolov@users.noreply.github.com>
2023-08-30 13:29:50 +0300

    Delete Inheritance/Hero directory

--- Inheritance/Hero/MuseElf.java
+++
@@ -1,7 +0,0 @@
-package Inheritance.exercise.hero;
-
-public class MuseElf extends Elf{
-    public MuseElf(String username, int level) {
-        super(username, level);
-    }
-}
--- Inheritance/Hero/Wizard.java
+++
@@ -1,7 +0,0 @@
-package Inheritance.exercise.hero;
-
-public class Wizard extends Hero{
-    public Wizard(String username, int level) {
-        super(username, level);
-    }
-}
